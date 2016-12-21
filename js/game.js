fc31d0c022a43c982c3f06c4e663af26fa431e0b
Sean McBride <spmcbride1201@gmail.com>
2016-12-21 10:01:58 -0500

    feat: add entities and draw logic.

--- js/game.js
+++ js/game.js
@@ -136,13 +136,221 @@ var levels = {
     {
       foreground: 'desert-foreground',
       background: 'clouds-background',
-      entities: []
+      entities: [
+        {
+          type: 'ground',
+          name: 'dirt',
+          x: 500,
+          y: 440,
+          width: 1000,
+          height: 20,
+          isStatic: true
+        },
+        {
+          type: 'ground',
+          name: 'wood',
+          x: 180,
+          y: 390,
+          width: 40,
+          height: 80,
+          isStatic: true
+        },
+        {
+          type: 'block',
+          name: 'wood',
+          x: 520,
+          y: 375,
+          angle: 90,
+          width: 100,
+          height: 25
+        },
+        {
+          type: 'block',
+          name: 'glass',
+          x: 520,
+          y: 275,
+          angle: 90,
+          width: 100,
+          height: 25
+        },
+        {
+          type: 'villain',
+          name: 'burger',
+          x: 520,
+          y: 200,
+          calories: 590
+        },
+        {
+          type: 'block',
+          name: 'wood',
+          x: 620,
+          y: 375,
+          angle: 90,
+          width: 100,
+          height: 25
+        },
+        {
+          type: 'block',
+          name: 'glass',
+          x: 620,
+          y: 275,
+          angle: 90,
+          width: 100,
+          height: 25
+        },
+        {
+          type: 'villain',
+          name: 'fries',
+          x: 620,
+          y: 200,
+          calories: 420
+        },
+        {
+          type: 'hero',
+          name: 'orange',
+          x: 90,
+          y: 410
+        },
+        {
+          type: 'hero',
+          name: 'apple',
+          x: 150,
+          y: 410
+        }
+      ]
     },
     // Level Two
     {
       foreground: 'desert-foreground',
       background: 'clouds-background',
-      entities: []
+      entities: [
+        {
+          type: 'ground',
+          name: 'dirt',
+          x: 500,
+          y: 440,
+          width: 1000,
+          height: 20,
+          isStatic: true
+        },
+        {
+          type: 'ground',
+          name: 'wood',
+          x: 180,
+          y: 390,
+          width: 40,
+          height: 80,
+          isStatic: true
+        },
+        {
+          type: 'block',
+          name: 'wood',
+          x: 820,
+          y: 375,
+          angle: 90,
+          width: 100,
+          height: 25
+        },
+        {
+          type: 'block',
+          name: 'wood',
+          x: 720,
+          y: 375,
+          angle: 90,
+          width: 100,
+          height: 25
+        },
+        {
+          type: 'block',
+          name: 'wood',
+          x: 620,
+          y: 375,
+          angle: 90,
+          width: 100,
+          height: 25
+        },
+        {
+          type: 'block',
+          name: 'glass',
+          x: 670,
+          y: 310,
+          width: 100,
+          height: 25
+        },
+        {
+          type: 'block',
+          name: 'glass',
+          x: 770,
+          y: 310,
+          width: 100,
+          height: 25
+        },
+        {
+          type: 'block',
+          name: 'glass',
+          x: 670,
+          y: 248,
+          angle: 90,
+          width: 100,
+          height: 25
+        },
+        {
+          type: 'block',
+          name: 'glass',
+          x: 770,
+          y: 248,
+          angle: 90,
+          width: 100,
+          height: 25
+        },
+        {
+          type: 'block',
+          name: 'wood',
+          x: 720,
+          y: 180,
+          width: 100,
+          height: 25
+        },
+        {
+          type: 'villain',
+          name: 'burger',
+          x: 715,
+          y: 160,
+          calories: 590
+        },
+        {
+          type: 'villain',
+          name: 'fries',
+          x: 670,
+          y: 400,
+          calories: 420
+        },
+        {
+          type: 'villain',
+          name: 'sodacan',
+          x: 765,
+          y: 395,
+          calories: 150
+        },
+        {
+          type: 'hero',
+          name: 'strawberry',
+          x: 40,
+          y: 420
+        },
+        {
+          type: 'hero',
+          name: 'orange',
+          x: 90,
+          y: 410
+        },
+        {
+          type: 'hero',
+          name: 'apple',
+          x: 150,
+          y: 410
+        }
+      ]
     }
   ],
   init: function () {
@@ -356,7 +564,44 @@ var entities = {
     }
   },
   // Turn an entity definition into a Box2D object and add to game world
-  create: function (entity) { },
+  create: function (entity) {
+    var definition = entities.definitions[entity.name];
+    if (!definition) {
+      console.log(entity.name, " is undefined");
+      return;
+    }
+    switch (entity.type) {
+      case "block":
+        entity.health = definition.fullHealth;
+        entity.fullHealth = definition.fullHealth;
+        entity.shape = 'rectangle'
+        entity.sprite = loader.loadImage('images/entities/' + entity.name + '.png');
+        box2d.createRectange(entity, definition);
+        break;
+      case "ground":
+        entity.shape = 'rectangle';
+        box2d.createRectange(entity, definition);
+        break;
+      case "hero":
+      case "villain":
+        entity.health = definition.fullHealth;
+        entity.fullHealth = definition.fullHealth;
+        entity.shape = definition.shape;
+        entity.sprite = loader.loadImage('images/entities/' + entity.name + '.png');
+        if (definition.shape === 'circle') {
+          entity.radius = definition.radius;
+          box2d.createCircle(entity, definition);
+        } else if (definition.shape === 'rectangle') {
+          entity.width = definition.width;
+          entity.height = definition.height;
+          box2d.createRectange(entity, definition);
+        }
+        break;
+      default:
+        console.log(entity.type + 'is undefined');
+        break;
+    }
+  },
   // Draw the entity on the canvas
   draw: function (entity, position, angle) { }
 }
